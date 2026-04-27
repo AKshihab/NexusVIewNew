f6e890f3021a77cdd3dedb4036d94438a11ebf4a
AKshihab <shihabkaiyumhossen@gmail.com>
2026-04-27 22:54:15 +0600

    Fix PyPI metadata and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "NexusVIewNew"
 AUTHOR_USER_NAME = "AK_SHIHAB"
 AUTHOR_EMAIL = "shihabkaiyumhossen@gmail.com"
@@ -15,13 +15,18 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package",
+
+    # ✅ IMPORTANT (fixes PyPI error)
     long_description=long_description,
     long_description_content_type="text/markdown",
+
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
+
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+
     python_requires=">=3.8",
 )
